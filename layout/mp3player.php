078d77df979a5f45d9cd49413128bfd7cf39943e
rogansi <joewtapper@gmail.com>
2013-12-03 23:29:54 -0600

    sadly, just uploading files to test for a different project at work.

---
+++ layout/mp3player.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="scripts/badmojo.js" type="text/javascript"></script>
+<meta charset="utf-8" />
+<title></title>
+</head>
+<body>
+<a href="sounds/test.mp3" 
+target="_blank" onclick="javascript:BatmoAudioPop('Mp3 Example','sounds/test.mp3','1'); 
+return false">
+<img src = "images/gaga.jpeg" alt = "gaga" /> </a>
+</body>
+</html>
